e5daccd01ceb0c31989c6af385ba833e480092b0
covalent <mail@lukehmcc.com>
2025-09-10 16:49:11 -0400

    part of one is working

--- HW1/src/bits.c
+++ HW1/src/bits.c
@@ -1,3 +1,69 @@
 #include "bits.h"
-unsigned int BinaryMirror(unsigned int input) { return 1; }
-unsigned int CountSequence(unsigned int input) { return 1; }
+#include <stdio.h>
+#include <stdlib.h>
+
+char *intToString(unsigned int n);
+unsigned int stringToInt(const char *binary);
+
+unsigned int BinaryMirror(unsigned int input) {
+  // First convert number into binary string (32 bit)
+  char *binary = intToString(input);
+  // give em a flip
+  char binaryFlipped[33];
+  for (int i = 31; i >= 0; i--) {
+    binaryFlipped[31 - i] = binary[i];
+  }
+  binaryFlipped[32] = '\0';
+  // Convert back into an int
+  unsigned int flipped = stringToInt(binaryFlipped);
+  // return
+  return flipped;
+}
+unsigned int CountSequence(unsigned int input) { // first
+  return 1;
+}
+
+char *intToString(unsigned int n) {
+  int c, k, i;
+  char *binary = (char *)malloc(33 * sizeof(char));
+  i = 0;
+
+  for (c = 31; c >= 0; c--) {
+    k = n >> c;
+
+    if (k & 1) {
+      binary[i] = '1';
+    } else {
+      binary[i] = '0';
+    }
+    i++;
+  }
+
+  binary[i] = '\0';
+
+  return binary;
+}
+
+unsigned int stringToInt(const char *binary) {
+  unsigned int result = 0;
+  int i = 0;
+
+  while (binary[i] == ' ' || binary[i] == '\t') {
+    i++;
+  }
+
+  while (binary[i] != '\0') {
+    result <<= 1;
+
+    if (binary[i] == '1') {
+      result |= 1;
+    } else if (binary[i] != '0') {
+      printf("Error: Invalid binary digit '%c' at position %d\n", binary[i], i);
+      return 0;
+    }
+
+    i++;
+  }
+
+  return result;
+}
--- HW1/src/main.c
+++ HW1/src/main.c
@@ -1,7 +1,46 @@
+#include "bits.h"
 #include <stdio.h>
 int main() {
   // Read in the ints to a list
-  FILE *file = fopen("./input.txt", "r");
-  fclose(file);
+  FILE *inFile = fopen("./input.txt", "r");
+  if (inFile == NULL) {
+    perror("Error opening inFile");
+    return 1;
+  }
+  // There are only 14 ints, were just going to assume it's less than 100
+  int integers[100];
+
+  // Loop through and grab the values
+  int i = 0;
+  int num;
+  while (fscanf(inFile, "%d", &num) > 0) {
+    integers[i] = num;
+    i++;
+  }
+  fclose(inFile);
+
+  // We now know how long it is so assign two lists to handle the new vals
+  int lengthOfInput = i;
+  int mirrors[lengthOfInput];
+  int countedSequences[lengthOfInput];
+
+  for (int i = 0; i < lengthOfInput; i++) {
+    mirrors[i] = BinaryMirror(integers[i]);
+    countedSequences[i] = CountSequence(integers[i]);
+  }
+
+  // Then close it out by writing those values out to a outFile
+  FILE *outFile = fopen("./output.txt", "w");
+  if (outFile == NULL) {
+    perror("Error opening outFile");
+    return 1;
+  }
+
+  // write out those values
+  for (int i = 0; i < lengthOfInput; i++) {
+    fprintf(outFile, "%d  > %d\n", mirrors[i], countedSequences[i]);
+  }
+
+  fclose(outFile);
   return 0;
 }
